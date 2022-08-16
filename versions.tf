f0380b27682abdce954c77bc1aff6231b5d76db3
PRAKASH KUMAR RADHAKRISHNAN <prakash@clumio.com>
2022-08-16 18:48:02 +1000

    Changed the provider requirements so that it can be passed in to the module. (#2)

--- versions.tf
+++ versions.tf
@@ -4,41 +4,8 @@ terraform {
       source  = "clumio-code/clumio"
       version = "~>0.3.0"
     }
-    aws = {}
+    aws = {
+      configuration_aliases = [ aws.uw1, aws.uw2, aws.ue1, aws.ue2, aws.cc1, aws.ew1, aws.ec1 ]
+    }
   }
-}
-
-provider aws {
-  region = "us-east-1"
-  alias = "ue1"
-}
-
-provider aws {
-  region = "us-east-2"
-  alias = "ue2"
-}
-
-provider aws {
-  region = "us-west-1"
-  alias = "uw1"
-}
-
-provider aws {
-  region = "us-west-2"
-  alias = "uw2"
-}
-
-provider aws {
-  region = "ca-central-1"
-  alias = "cc1"
-}
-
-provider aws {
-  region = "eu-central-1"
-  alias = "ec1"
-}
-
-provider aws {
-  region = "eu-west-1"
-  alias = "ew1"
 }
