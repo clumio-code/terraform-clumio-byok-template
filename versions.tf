c940b046007f4cf62d9b64441e17522007b9f766
Deepak Chawla <deepak@clumio.com>
2022-08-28 00:45:17 -0700

    Changed the BYOK module to create an IAM role and only one CMK

--- versions.tf
+++ versions.tf
@@ -4,8 +4,7 @@ terraform {
       source  = "clumio-code/clumio"
       version = "~>0.3.0"
     }
-    aws = {
-      configuration_aliases = [ aws.uw1, aws.uw2, aws.ue1, aws.ue2, aws.cc1, aws.ew1, aws.ec1 ]
-    }
+    aws = {}
+    random = {}
   }
 }
