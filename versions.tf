41aed0cbc9569fd2e3c3a8765a97d0a9b45d0afc
PRAKASH KUMAR RADHAKRISHNAN <prakash@clumio.com>
2024-05-17 14:11:43 +1000

    Provider version update (#11)

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     clumio = {
       source  = "clumio-code/clumio"
-      version = ">=0.5.1, <0.7.0"
+      version = ">=0.6.0, <0.8.0"
     }
     aws = {}
     random = {}
