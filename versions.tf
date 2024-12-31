9d3f53502847ce68cd04c98121b15c28df229f4a
Dongjoo Kim <dongjoo@clumio.com>
2024-12-31 16:36:10 +0900

    Required provider version updated to >=0.11.0, <0.13.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     clumio = {
       source  = "clumio-code/clumio"
-      version = ">=0.10.0, <0.12.0"
+      version = ">=0.11.0, <0.13.0"
     }
     aws = {}
     random = {}
